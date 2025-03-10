203238fa742a3129af1a38986aef81cd86058610
Disha Kamble <120121137+dishak24@users.noreply.github.com>
2025-03-10 13:14:09 +0530

    OOPs  Concepts

---
+++ OOPs/Inheritance/Animal.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inheritance
+{
+    public class Animal
+    {
+        /*public String Name 
+        { 
+            get; 
+            set; 
+        }*/
+
+        public Animal()
+        {
+            Console.WriteLine("Animal Class Constructor");
+        }
+
+        ~Animal()
+        {
+            Console.WriteLine("Animal Destructor");
+        }
+
+    }
+
+    public class Dog : Animal
+    {
+        public Dog()
+        {
+            Console.WriteLine("In Dog Constructor ");
+        }
+
+        ~Dog()
+        {
+            Console.WriteLine("Dog Destructor");
+        }
+
+    }
+
+
+
+}
---
+++ OOPs/Inheritance/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Inheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("In main()");
+            //Animal obj = new Animal();
+            Dog obj = new Dog ();
+        }
+    }
+}
